9f1a71a190673245d1a080a376744e71a86fc8f2
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-17 15:43:27 +0200

    refactor: improve indentation of sql code in populatedb.js

--- db/populatedb.js
+++ db/populatedb.js
@@ -61,289 +61,294 @@ const queries = require("./queries.js");
 //   Columns: category_id, actual_for_pizzas_ids, actual_for_pizzas_names, pizzas_ids, pizzas_names, enforced_in_pizzas_ids, incompatible_with_pizzas_ids, enforced_in_pizzas_names, incompatible_with_pizzas_names
 
 const defaultColumns = `
-        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        name VARCHAR(${Number(process.env.NAME_MAX_LENGTH)}) UNIQUE NOT NULL,
-        is_protected BOOLEAN NOT NULL DEFAULT FALSE,
-        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMPTZ DEFAULT NULL,
-        notes VARCHAR(${Number(process.env.NOTES_MAX_LENGTH)})`;
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(${Number(process.env.NAME_MAX_LENGTH)}) UNIQUE NOT NULL,
+  is_protected BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT NULL,
+  notes VARCHAR(${Number(process.env.NOTES_MAX_LENGTH)})`;
 
 const createForeignKeyColumn = (id, tab, tabId = "id") => `
-        ${id} INTEGER NOT NULL,
-        FOREIGN KEY (${id}) REFERENCES ${tab}(${tabId})`;
+  ${id} INTEGER NOT NULL,
+  FOREIGN KEY (${id}) REFERENCES ${tab}(${tabId})`;
 
 const SQL_drop = `
-    DROP VIEW IF EXISTS category_rules_per_ingredient;
-    DROP VIEW IF EXISTS ingredient_rules_per_category;
-    DROP VIEW IF EXISTS ingredients_per_pizza;
-    DROP VIEW IF EXISTS pizzas_per_ingredient;
-    DROP VIEW IF EXISTS categories_per_pizza;
-    DROP VIEW IF EXISTS pizzas_per_category;
-    DROP VIEW IF EXISTS pizzas_actual_categories;
-    DROP VIEW IF EXISTS pizzas_categories_rules;
+  DROP VIEW IF EXISTS category_rules_per_ingredient;
+  DROP VIEW IF EXISTS ingredient_rules_per_category;
+  DROP VIEW IF EXISTS ingredients_per_pizza;
+  DROP VIEW IF EXISTS pizzas_per_ingredient;
+  DROP VIEW IF EXISTS categories_per_pizza;
+  DROP VIEW IF EXISTS pizzas_per_category;
+  DROP VIEW IF EXISTS pizzas_actual_categories;
+  DROP VIEW IF EXISTS pizzas_categories_rules;
 
-    DROP TABLE IF EXISTS pizzas_categories;
-    DROP TABLE IF EXISTS pizzas_ingredients;
-    DROP TABLE IF EXISTS ingredients_categories_rules;
-    DROP TABLE IF EXISTS pizzas;
-    DROP TABLE IF EXISTS categories;
-    DROP TABLE IF EXISTS ingredients;
+  DROP TABLE IF EXISTS pizzas_categories;
+  DROP TABLE IF EXISTS pizzas_ingredients;
+  DROP TABLE IF EXISTS ingredients_categories_rules;
+  DROP TABLE IF EXISTS pizzas;
+  DROP TABLE IF EXISTS categories;
+  DROP TABLE IF EXISTS ingredients;
 `;
 
 const SQL_create = `
-    CREATE TABLE IF NOT EXISTS pizzas(
-        ${defaultColumns}
-    );
+  CREATE TABLE IF NOT EXISTS pizzas(
+    ${defaultColumns}
+  );
 
-    CREATE TABLE IF NOT EXISTS categories(
-        ${defaultColumns}
-    );
+  CREATE TABLE IF NOT EXISTS categories(
+    ${defaultColumns}
+  );
 
-    CREATE TABLE IF NOT EXISTS ingredients (
-        ${defaultColumns},
-        price NUMERIC NOT NULL DEFAULT 1,
-        stock INTEGER NOT NULL DEFAULT 100
-    );
+  CREATE TABLE IF NOT EXISTS ingredients (
+    ${defaultColumns},
+    price NUMERIC NOT NULL DEFAULT 1,
+    stock INTEGER NOT NULL DEFAULT 100
+  );
 
-    CREATE TABLE IF NOT EXISTS pizzas_categories (
-        ${createForeignKeyColumn("pizza_id", "pizzas")},
-        ${createForeignKeyColumn("category_id", "categories")},
-        CONSTRAINT U_pizza_category UNIQUE (pizza_id,category_id)
-    );
+  CREATE TABLE IF NOT EXISTS pizzas_categories (
+    ${createForeignKeyColumn("pizza_id", "pizzas")},
+    ${createForeignKeyColumn("category_id", "categories")},
+    CONSTRAINT U_pizza_category UNIQUE (pizza_id,category_id)
+  );
 
-    CREATE TABLE IF NOT EXISTS pizzas_ingredients (
-        ${createForeignKeyColumn("pizza_id", "pizzas")},
-        ${createForeignKeyColumn("ingredient_id", "ingredients")},
-        CONSTRAINT U_pizza_ingredient UNIQUE (pizza_id, ingredient_id)
-    );
+  CREATE TABLE IF NOT EXISTS pizzas_ingredients (
+    ${createForeignKeyColumn("pizza_id", "pizzas")},
+    ${createForeignKeyColumn("ingredient_id", "ingredients")},
+    CONSTRAINT U_pizza_ingredient UNIQUE (pizza_id, ingredient_id)
+  );
 
-    CREATE TABLE IF NOT EXISTS ingredients_categories_rules (
-        ${createForeignKeyColumn("ingredient_id", "ingredients")},
-        ${createForeignKeyColumn("category_id", "categories")},
-        rule_type VARCHAR(30) NOT NULL CHECK (rule_type IN ('enforcing', 'incompatible')),
-        CONSTRAINT U_ingredient_category UNIQUE (ingredient_id,category_id)
-    );
+  CREATE TABLE IF NOT EXISTS ingredients_categories_rules (
+    ${createForeignKeyColumn("ingredient_id", "ingredients")},
+    ${createForeignKeyColumn("category_id", "categories")},
+    rule_type VARCHAR(30) NOT NULL CHECK (rule_type IN ('enforcing', 'incompatible')),
+    CONSTRAINT U_ingredient_category UNIQUE (ingredient_id,category_id)
+  );
 
-    CREATE VIEW pizzas_categories_rules AS
-    SELECT DISTINCT 
-        pizza_id,
-        category_id, 
-        rule_type
-    FROM pizzas_ingredients AS pi
-    JOIN ingredients_categories_rules AS ci  -- ignore ingredients with no rules
-    ON pi.ingredient_id = ci.ingredient_id;
+  CREATE VIEW pizzas_categories_rules AS
+  SELECT DISTINCT 
+    pizza_id,
+    category_id, 
+    rule_type
+  FROM pizzas_ingredients AS pi
+  JOIN ingredients_categories_rules AS ci  -- ignore ingredients with no rules
+  ON pi.ingredient_id = ci.ingredient_id;
 
-
-    CREATE VIEW pizzas_actual_categories AS
-    SELECT 
-    	pizza_id,
-	    category_id AS actual_category_id
-    FROM ((
-      (
-        SELECT pizza_id,
-               category_id
-        FROM pizzas_categories_rules
-        WHERE rule_type = 'enforcing'
-      ) UNION (
-        SELECT *
-        FROM pizzas_categories
-      )
-    ) EXCEPT (
+  CREATE VIEW pizzas_actual_categories AS
+  SELECT 
+  	pizza_id,
+	  category_id AS actual_category_id
+  FROM ((
+    (
       SELECT pizza_id,
              category_id
       FROM pizzas_categories_rules
-      WHERE rule_type = 'incompatible'
-    ));
+      WHERE rule_type = 'enforcing'
+    ) UNION (
+      SELECT *
+      FROM pizzas_categories
+    )
+  ) EXCEPT (
+    SELECT pizza_id,
+           category_id
+    FROM pizzas_categories_rules
+    WHERE rule_type = 'incompatible'
+  ));
 
 	CREATE VIEW category_rules_per_ingredient AS
 	SELECT 
 	  ingredient_id, 
 	  array_remove(
-		ARRAY_AGG(
-		  CASE WHEN rule_type = 'enforcing' THEN category_id END 
-		  ORDER BY category_id
-		), NULL
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'enforcing' THEN category_id END 
+        ORDER BY category_id
+      ), NULL
 	  ) AS enforced_categories_ids, 
 	  array_remove(
-		ARRAY_AGG(
-		  CASE WHEN rule_type = 'incompatible' THEN category_id END 
-		  ORDER BY category_id
-		), NULL
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'incompatible' THEN category_id END 
+        ORDER BY category_id
+      ), NULL
 	  ) AS incompatible_categories_ids,
-      array_remove(
-		ARRAY_AGG(
-		  CASE WHEN rule_type = 'enforcing' THEN c.name END 
-		  ORDER BY category_id
-		), NULL
+    array_remove(
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'enforcing' THEN c.name END 
+        ORDER BY category_id
+      ), NULL
 	  ) AS enforced_categories_names, 
 	  array_remove(
-		ARRAY_AGG(
-		  CASE WHEN rule_type = 'incompatible' THEN c.name END 
-		  ORDER BY category_id
-		), NULL
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'incompatible' THEN c.name END 
+        ORDER BY category_id
+      ), NULL
 	  ) AS incompatible_categories_names  
 	FROM ingredients_categories_rules AS ic
-    LEFT JOIN categories AS c
-    ON ic.category_id = c.id
+  LEFT JOIN categories AS c
+  ON ic.category_id = c.id
 	GROUP BY ingredient_id;
 
-    CREATE VIEW ingredient_rules_per_category AS
-    SELECT 
-      category_id, 
-      array_remove(
-        ARRAY_AGG(
-          CASE WHEN rule_type = 'enforcing' THEN ingredient_id END 
-          ORDER BY ingredient_id
-        ), NULL
-      ) AS enforcing_ingredients_ids, 
-      array_remove(
-        ARRAY_AGG(
-          CASE WHEN rule_type = 'incompatible' THEN ingredient_id END 
-          ORDER BY ingredient_id
-        ), NULL
-      ) AS incompatible_ingredients_ids,
-      array_remove(
-        ARRAY_AGG(
-          CASE WHEN rule_type = 'enforcing' THEN i.name END 
-          ORDER BY ingredient_id
-        ), NULL
-      ) AS enforcing_ingredients_names, 
-      array_remove(
-        ARRAY_AGG(
-          CASE WHEN rule_type = 'incompatible' THEN i.name END 
-          ORDER BY ingredient_id
-        ), NULL
-      ) AS incompatible_ingredients_names  
-    FROM ingredients_categories_rules AS ic
-    LEFT JOIN ingredients AS i
-    ON ic.ingredient_id = i.id
-    GROUP BY category_id;
+  CREATE VIEW ingredient_rules_per_category AS
+  SELECT 
+    category_id, 
+    array_remove(
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'enforcing' THEN ingredient_id END 
+        ORDER BY ingredient_id
+      ), NULL
+    ) AS enforcing_ingredients_ids, 
+    array_remove(
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'incompatible' THEN ingredient_id END 
+        ORDER BY ingredient_id
+      ), NULL
+    ) AS incompatible_ingredients_ids,
+    array_remove(
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'enforcing' THEN i.name END 
+        ORDER BY ingredient_id
+      ), NULL
+    ) AS enforcing_ingredients_names, 
+    array_remove(
+      ARRAY_AGG(
+        CASE WHEN rule_type = 'incompatible' THEN i.name END 
+        ORDER BY ingredient_id
+      ), NULL
+    ) AS incompatible_ingredients_names  
+  FROM ingredients_categories_rules AS ic
+  LEFT JOIN ingredients AS i
+  ON ic.ingredient_id = i.id
+  GROUP BY category_id;
 
-    CREATE VIEW ingredients_per_pizza AS
-    SELECT 
-      pi.pizza_id, 
-      ARRAY_AGG(pi.ingredient_id ORDER BY pi.ingredient_id) AS ingredients_ids,
-      ARRAY_AGG(i.name ORDER BY pi.ingredient_id) AS ingredients_names,
-      ARRAY_AGG(i.price ORDER BY pi.ingredient_id) AS ingredients_prices,
-      ARRAY_AGG(i.stock ORDER BY pi.ingredient_id) AS ingredients_stocks,
-      SUM(i.price) AS ingredients_total_cost,
-      MIN(i.stock) AS availability
-    FROM pizzas_ingredients AS pi
-    LEFT JOIN ingredients AS i
-    ON pi.ingredient_id = i.id
-    GROUP BY pizza_id;
+  CREATE VIEW ingredients_per_pizza AS
+  SELECT 
+    pi.pizza_id, 
+    ARRAY_AGG(pi.ingredient_id ORDER BY pi.ingredient_id) AS ingredients_ids,
+    ARRAY_AGG(i.name ORDER BY pi.ingredient_id) AS ingredients_names,
+    ARRAY_AGG(i.price ORDER BY pi.ingredient_id) AS ingredients_prices,
+    ARRAY_AGG(i.stock ORDER BY pi.ingredient_id) AS ingredients_stocks,
+    SUM(i.price) AS ingredients_total_cost,
+    MIN(i.stock) AS availability
+  FROM pizzas_ingredients AS pi
+  LEFT JOIN ingredients AS i
+  ON pi.ingredient_id = i.id
+  GROUP BY pizza_id;
 
-    CREATE VIEW pizzas_per_ingredient AS
-    SELECT 
-      pi.ingredient_id, 
-      ARRAY_AGG(pi.pizza_id ORDER BY pi.pizza_id) AS pizzas_ids,
-      ARRAY_AGG(p.name ORDER BY pi.pizza_id) AS pizzas_names
-    FROM pizzas_ingredients AS pi
-    LEFT JOIN pizzas AS p
-    ON pi.pizza_id = p.id
-    GROUP BY ingredient_id;
+  CREATE VIEW pizzas_per_ingredient AS
+  SELECT 
+    pi.ingredient_id, 
+    ARRAY_AGG(pi.pizza_id ORDER BY pi.pizza_id) AS pizzas_ids,
+    ARRAY_AGG(p.name ORDER BY pi.pizza_id) AS pizzas_names
+  FROM pizzas_ingredients AS pi
+  LEFT JOIN pizzas AS p
+  ON pi.pizza_id = p.id
+  GROUP BY ingredient_id;
 
-    CREATE VIEW categories_per_pizza AS
-    SELECT * -- or explicit, using:  COALESCE(pac_c.pizza_id, pc_c.pizza_id, pcr_c.pizza_id) AS pizza_id, ...
-    FROM (
-      SELECT 
-        pizza_id, 
-        ARRAY_AGG(actual_category_id ORDER BY actual_category_id) AS actual_categories_ids,
-        ARRAY_AGG(c.name ORDER BY actual_category_id) AS actual_categories_names
-      FROM pizzas_actual_categories AS pac
-      LEFT JOIN categories AS c
-      ON pac.actual_category_id = c.id
-      GROUP BY pizza_id
-    ) AS pac_c
-    FULL JOIN (
-      SELECT 
-        pizza_id, 
-        ARRAY_AGG(category_id ORDER BY category_id) AS categories_ids,
-        ARRAY_AGG(c.name ORDER BY category_id) AS categories_names
-      FROM pizzas_categories AS pc
-      LEFT JOIN categories AS c
-      ON pc.category_id = c.id
-      GROUP BY pizza_id
-    ) AS pc_c
-    USING(pizza_id) -- this is a common column, and is merged
-    FULL JOIN (
-      SELECT 
-        pizza_id,
-        array_remove(
+  CREATE VIEW categories_per_pizza AS
+  SELECT * -- or explicit, using:  COALESCE(pac_c.pizza_id, pc_c.pizza_id, pcr_c.pizza_id) AS pizza_id, ...
+  FROM (
+    SELECT 
+      pizza_id, 
+      ARRAY_AGG(actual_category_id ORDER BY actual_category_id) AS actual_categories_ids,
+      ARRAY_AGG(c.name ORDER BY actual_category_id) AS actual_categories_names
+    FROM pizzas_actual_categories AS pac
+    LEFT JOIN categories AS c
+    ON pac.actual_category_id = c.id
+    GROUP BY pizza_id
+  ) AS pac_c
+  FULL JOIN (
+    SELECT 
+      pizza_id, 
+      ARRAY_AGG(category_id ORDER BY category_id) AS categories_ids,
+      ARRAY_AGG(c.name ORDER BY category_id) AS categories_names
+    FROM pizzas_categories AS pc
+    LEFT JOIN categories AS c
+    ON pc.category_id = c.id
+    GROUP BY pizza_id
+  ) AS pc_c
+  USING(pizza_id) -- this is a common column, and is merged
+  FULL JOIN (
+    SELECT 
+      pizza_id,
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'enforcing' THEN category_id END 
           ORDER BY category_id
-        ), NULL) AS enforced_categories_ids, 
-        array_remove(
+        ), NULL
+      ) AS enforced_categories_ids, 
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'incompatible' THEN category_id END 
           ORDER BY category_id
-        ), NULL) AS incompatible_categories_ids,
-        array_remove(
+        ), NULL
+      ) AS incompatible_categories_ids,
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'enforcing' THEN c.name END 
           ORDER BY category_id
-        ), NULL) AS enforced_categories_names, 
-        array_remove(
+        ), NULL
+      ) AS enforced_categories_names, 
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'incompatible' THEN c.name END 
           ORDER BY category_id
         ), NULL
-        ) AS incompatible_categories_names  	   
+      ) AS incompatible_categories_names  	   
       FROM pizzas_categories_rules AS pc
       LEFT JOIN categories AS c
       ON pc.category_id = c.id
       GROUP BY pizza_id
     ) AS pcr_c
-    USING(pizza_id); -- or ON pcr_c.pizza_id = COALESCE(pac_c.pizza_id,pc_c.pizza_id);
+  USING(pizza_id); -- or ON pcr_c.pizza_id = COALESCE(pac_c.pizza_id,pc_c.pizza_id);
 
   CREATE VIEW pizzas_per_category AS	
 	SELECT *
-    FROM (
-      SELECT 
-        pac.actual_category_id AS category_id, 
-        ARRAY_AGG(p.id ORDER BY p.id) AS actual_for_pizzas_ids,
-        ARRAY_AGG(p.name ORDER BY p.id) AS actual_for_pizzas_names
-      FROM pizzas_actual_categories AS pac
-      LEFT JOIN pizzas AS p
-      ON pac.pizza_id = p.id
-      GROUP BY category_id
-    ) AS pac_p
-    FULL JOIN (
-      SELECT 
-        category_id, 
-        ARRAY_AGG(p.id ORDER BY p.id) AS pizzas_ids,
-        ARRAY_AGG(p.name ORDER BY p.id) AS pizzas_names
-      FROM pizzas_categories AS pc
-      LEFT JOIN pizzas AS p
-      ON pc.pizza_id = p.id
-      GROUP BY category_id
-	) AS pc_p
-    USING(category_id)
-    FULL JOIN (
-      SELECT 
-        category_id,
-        array_remove(
+  FROM (
+    SELECT 
+      pac.actual_category_id AS category_id, 
+      ARRAY_AGG(p.id ORDER BY p.id) AS actual_for_pizzas_ids,
+      ARRAY_AGG(p.name ORDER BY p.id) AS actual_for_pizzas_names
+    FROM pizzas_actual_categories AS pac
+    LEFT JOIN pizzas AS p
+    ON pac.pizza_id = p.id
+    GROUP BY category_id
+  ) AS pac_p
+  FULL JOIN (
+    SELECT 
+      category_id, 
+      ARRAY_AGG(p.id ORDER BY p.id) AS pizzas_ids,
+      ARRAY_AGG(p.name ORDER BY p.id) AS pizzas_names
+    FROM pizzas_categories AS pc
+    LEFT JOIN pizzas AS p
+    ON pc.pizza_id = p.id
+    GROUP BY category_id
+  ) AS pc_p
+  USING(category_id)
+  FULL JOIN (
+    SELECT 
+      category_id,
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'enforcing' THEN p.id END 
           ORDER BY p.id
-        ), NULL) AS enforced_in_pizzas_ids, 
-        array_remove(
+        ), NULL
+      ) AS enforced_in_pizzas_ids, 
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'incompatible' THEN p.id END 
           ORDER BY p.id
-        ), NULL) AS incompatible_with_pizzas_ids,
-        array_remove(
+        ), NULL
+      ) AS incompatible_with_pizzas_ids,
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'enforcing' THEN p.name END 
           ORDER BY p.id
-        ), NULL) AS enforced_in_pizzas_names, 
-        array_remove(
+        ), NULL
+      ) AS enforced_in_pizzas_names, 
+      array_remove(
         ARRAY_AGG(
           CASE WHEN rule_type = 'incompatible' THEN p.name END 
           ORDER BY p.id
         ), NULL
-        ) AS incompatible_with_pizzas_names  	   
+      ) AS incompatible_with_pizzas_names  	   
       FROM pizzas_categories_rules AS pc
       LEFT JOIN pizzas AS p
       ON pc.pizza_id = p.id
