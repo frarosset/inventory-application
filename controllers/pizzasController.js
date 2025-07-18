f0c112b9617b5906c05969aaa58aff4a000c100c
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-18 12:13:21 +0200

    feat: create view for /pizzas route

--- controllers/pizzasController.js
+++ controllers/pizzasController.js
@@ -3,5 +3,6 @@ const asyncHandler = require("express-async-handler");
 
 exports.get = asyncHandler(async (req, res) => {
   const pizzasBriefData = await db.read.pizzasBrief();
-  res.send(JSON.stringify(pizzasBriefData));
+
+  res.render("pizzas", { title: process.env.TITLE, pizzasBriefData });
 });
