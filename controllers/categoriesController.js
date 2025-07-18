99bca6a34660b550c87ed86b2a1b8baf715bbbb0
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-18 16:37:32 +0200

    feat: create view for /categories route

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -4,5 +4,5 @@ const asyncHandler = require("express-async-handler");
 exports.get = asyncHandler(async (req, res) => {
   const categoriesBriefData = await db.read.categoriesBrief();
 
-  res.send("categories" + JSON.stringify(categoriesBriefData));
+  res.render("categories", { title: process.env.TITLE, categoriesBriefData });
 });
