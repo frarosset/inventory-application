5d9a5a2d5b9ef0167832c4eb538019ad419af791
frarosset <37275581+frarosset@users.noreply.github.com>
2025-09-15 16:06:49 +0200

    feat: shrink responsive tables only when they overflow horizontally
    Previously, tables with responsive class were shrunk based on some media
    query tied to the window width.
    
    However, this does not take into account the fact that some tables might
    need to shrink at larger or shorter widths based on the width of the
    tables.
    
    Now a js script is introduced to compute whether each table with
    responsive class overflows horizontally, and in that case a shrink class
    is applied, which is used to apply the proper styles without media
    queries.

---
+++ public/js/checkResponsiveTables.js
@@ -0,0 +1,26 @@
+(() => {
+  const tabs = document.querySelectorAll("table.responsive");
+
+  const checkResponsiveTables = () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("shrink");
+
+      if (tab.scrollWidth > tab.clientWidth) {
+        console.log("Horizontal overflow detected");
+        tab.classList.add("shrink");
+      } else {
+        console.log("No horizontal overflow");
+      }
+
+      console.log(tab.scrollWidth, tab.clientWidth);
+    });
+  };
+
+  const observer = new ResizeObserver(checkResponsiveTables);
+
+  observer.observe(document.body, {
+    box: "border-box",
+  });
+
+  checkResponsiveTables();
+})();
